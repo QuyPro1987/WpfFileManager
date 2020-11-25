c98b840d85ab62c08d6e34f86b50fa77cbded3b6
QuyLD <ledinhquy2741987@gmail.com>
2020-11-25 20:50:25 +0700

    add file

--- FileManager/MainWindow.xaml.cs
+++ FileManager/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FileManager.View;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,8 @@ namespace FileManager
         public MainWindow()
         {
             InitializeComponent();
+            Login loginView = new Login();
+            loginView.Show();
         }
     }
 }
