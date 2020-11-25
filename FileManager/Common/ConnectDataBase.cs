6137bcfba27a54bd7801c9364b7a20485a821889
QuyPro1987 <ledinhquy2741987@gmail.com>
2020-11-25 12:55:22 +0700

    add file

---
+++ FileManager/Common/ConnectDataBase.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileManager.Common
+{
+    class ConnectDataBase
+    {
+        public static SqlConnection GetDBConnection()
+        {
+            var connection = System.Configuration.ConfigurationManager.ConnectionStrings["DBConnectionString"].ConnectionString;
+            SqlConnection conn = new SqlConnection(connection);
+            return conn;
+        }
+    }
+}
